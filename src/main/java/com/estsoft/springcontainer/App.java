2c2e263c19b72241f3fac11da44cdeb09dc6d715
kickscar <kickscar@gmail.com>
2016-05-02 09:33:51 +0900

    bean 설정 - 생성자, 필드(setter/getter)

--- src/main/java/com/estsoft/springcontainer/App.java
+++ src/main/java/com/estsoft/springcontainer/App.java
@@ -3,6 +3,7 @@ package com.estsoft.springcontainer;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
@@ -27,11 +28,22 @@ public class App {
     	ApplicationContext applicationContext = 
     			new ClassPathXmlApplicationContext( "config/applicationContext.xml" );
     	
-    	User user1 = applicationContext.getBean( User.class );
-    	user1.setName( "안대혁" );
+ //   	User user1 = applicationContext.getBean( User.class );
+ //   	user1.setName( "안대혁" );
+ //   	System.out.println( user1 );
+    	
+    	User user1 = (User)applicationContext.getBean( "user1" );
     	System.out.println( user1 );
     	
-    	User user2 = (User)applicationContext.getBean( "user1" );
+    	User user2 = (User)applicationContext.getBean( "user2" );
     	System.out.println( user2 );
+    	
+    	User user3 = (User)applicationContext.getBean( "user3" );
+    	System.out.println( user3 );
+
+    	User user4 = (User)applicationContext.getBean( "user4" );
+    	System.out.println( user4 );
+
+    	( ( ConfigurableApplicationContext ) applicationContext ).close();
     }
 }
--- src/main/java/com/estsoft/springcontainer/User.java
+++ src/main/java/com/estsoft/springcontainer/User.java
@@ -1,13 +1,22 @@
 package com.estsoft.springcontainer;
 
-import org.springframework.stereotype.Component;
-
-@Component
+//@Component
 public class User {
 	public String name;
+	public String email;
+	
+	private Friend friend;
+	
+	public User() {
+	}
 	
 	public User( String name ) {
 		this.name = name;
+	}
+
+	public User( String name, String email ) {
+		this.name = name;
+		this.email = email;
 	}
 	
 	public String getName() {
@@ -17,9 +26,27 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Friend getFriend() {
+		return friend;
+	}
+
+	public void setFriend(Friend friend) {
+		this.friend = friend;
+	}
 
 	@Override
 	public String toString() {
-		return "User [name=" + name + "]";
+		return "User [name=" + name + ", email=" + email + ", friend=" + friend + "]";
 	}
+
+
 }
